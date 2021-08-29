dbdaa6989a060ceaf96d033b6434ef52323e53f3
0L1V31R <eng.samuel@ymail.com>
2021-08-29 14:51:50 -0300

    created component and started project

---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course {
+    id: number = 0;
+    name: string = 'a';
+    imageUrl: string = '';
+    price: number = 0;
+    code: string = '';
+    duration: number = 1;
+    rating: number = 6;
+    releaseDate: string = '';
+}
